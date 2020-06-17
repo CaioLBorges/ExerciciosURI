3fd4cb909d425d25c3e8b0e31ec534ab77487b99
Caio de Lima Borges <caiolborges@hotmail.com>
2020-06-16 22:28:07 -0300

    Primeiros 03 exercícios

---
+++ URI/URI1002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A fórmula para calcular a área de uma circunferência é: area = π.raio2.Considerando para este problema que π = 3.14159:
+            //-Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+            double area, raio;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = 3.14159 * Math.Pow(raio, 2);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ URI/URI1003/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA.
+            //A seguir escrever o valor desta variável.
+
+            int A, B, SOMA;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+        }
+    }
+}
