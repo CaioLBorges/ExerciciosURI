e1659c78f2c623bc7767dc137f0481c65baa88ea
Caio de Lima Borges <caiolborges@hotmail.com>
2020-06-21 22:40:18 -0300

    Atualização

---
+++ URI/URI 1019/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace URI_1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, 
+            //e informe - o expresso no formato horas:minutos: segundos.
+            // 1hora = 60min = 3600segundos
+
+            int N, horas, minutos, segundos;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            minutos = (N % 3600) / 60;
+            segundos = (N % 3600) % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
