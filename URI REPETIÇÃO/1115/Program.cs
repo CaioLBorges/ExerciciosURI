3c5a2b511b40487d8b21fffc2cf1aea54246ff96
Caio de Lima Borges <caiolborges@hotmail.com>
2021-01-27 10:49:27 -0300

    2701 01

---
+++ URI REPETIÇÃO/1115/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vs = Console.ReadLine().Split(' ');
+            int x = int.Parse(vs[0]);
+            int y = int.Parse(vs[1]);
+
+            while(x != 0 && y != 0)
+            {
+                if (x > 0)
+                {
+                    if(y > 0)
+                    {
+                        Console.WriteLine("primeiro");
+                    }
+                    else
+                    {
+                        Console.WriteLine("quarto");
+                    }
+                }
+                else
+                {
+                    if (y > 0)
+                    {
+                        Console.WriteLine("segundo");
+                    }
+                    else
+                    {
+                        Console.WriteLine("terceiro");
+                    }
+                }
+                vs = Console.ReadLine().Split(' ');
+                x = int.Parse(vs[0]);
+                y = int.Parse(vs[1]);
+
+            }
+        }
+    }
+}
---
+++ URI REPETIÇÃO/URI 3.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace URI_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*estrutura básica para comando enquanto, onde não se sabe quantidade de vezes que irá repetir, teste feito antes de executar comando*/
+            int x, soma = 0;
+
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+                soma += x;
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("A SOMA É: " + soma);
+        }
+    }
+}
