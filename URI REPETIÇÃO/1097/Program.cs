58914cac68a8cb08adbf746532e2363eb26a9b75
Caio de Lima Borges <caiolborges@hotmail.com>
2021-01-29 18:53:46 -0300

    2901 01

---
+++ URI REPETIÇÃO/1097/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int k = 0;
+            for(int i = 1; i <= 9; i += 2)
+            {
+                for(int j = 7; j >= 5; j--)
+                {
+                    Console.WriteLine("I=" + i + " J=" + (j + k));
+                }
+                k += 2; 
+            }*/
+            int j = 7;
+            for (int i = 1; i <= 9; i += 2)
+            {
+                Console.WriteLine("I=" + i + " J=" + j);
+                Console.WriteLine("I=" + i + " J=" + (j - 1));
+                Console.WriteLine("I=" + i + " J=" + (j - 2));
+                j += 2;
+            }
+
+
+        }
+    }
+}
