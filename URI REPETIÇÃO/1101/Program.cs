bda452309bf6447272f3ff5bcbe60dad8c1d63d1
Caio de Lima Borges <caiolborges@hotmail.com>
2021-01-31 11:30:45 -0300

    3101 01

---
+++ URI REPETIÇÃO/1101/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vs = Console.ReadLine().Split(' ');
+            int A = int.Parse(vs[0]);
+            int B = int.Parse(vs[1]);
+
+            while (A > 0 && B > 0)
+            {
+                int soma = 0;
+
+                if (A > B)
+                {
+                    int organizadora = B;
+                    B = A;
+                    A = organizadora;
+                }
+                for (int i = A; i <= B; i++)
+                {
+                    Console.Write(i + " ");
+                    soma += i;
+                }
+                Console.WriteLine("Sum=" + soma);
+                vs = Console.ReadLine().Split(' ');
+                A = int.Parse(vs[0]);
+                B = int.Parse(vs[1]);
+            }
+        }
+    }
+}
---
+++ URI REPETIÇÃO/1145/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vs = Console.ReadLine().Split(' ');
+            int X = int.Parse(vs[0]);
+            int Y = int.Parse(vs[1]);
+
+            if (X > 1 && X < 20 && Y > X && Y < 100000)
+            {
+                for (int i = 1; i <= Y; i++)
+                {
+                    for(int j = 1; j < X; j++)
+                    {
+                        Console.Write(i + " ");
+                        i++;
+                    }
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
