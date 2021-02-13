02dbd2b1aff6f9c52e268b3a85a0191ffb8704df
Caio de Lima Borges <caiolborges@hotmail.com>
2021-02-13 15:12:43 -0300

    1302 01

---
+++ URI REPETIÇÃO/MAT01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MAT01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M, N;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            M = int.Parse(vet[0]);
+            N = int.Parse(vet[1]);
+            int[,] matriz = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                vet = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    matriz[i, j] = int.Parse(vet[j]);
+                }
+            }
+            Console.WriteLine("VALORES NEGATIVOS:");
+            
+            for (int i =0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (matriz[i, j] < 0)
+                    {
+                        Console.WriteLine(matriz[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
